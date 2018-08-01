7229245037cb8c3548bedb9035ced626d0c22cbf
Hans Olav <loftum@gmail.com>
2018-08-01 22:15:49 +0200

    ServoTest

---
+++ dotnet/ServoTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Devices._4tronix;
+using Unosquare.RaspberryIO;
+
+namespace ServoTest
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine($"Hello {Pi.Info}");
+            var picon = new PiconZeroBoard(Pi.I2C);
+            Console.WriteLine(picon.GetRevision());
+            var running = true;
+            Console.CancelKeyPress += (s, a) => running = false;
+            using (var servo = new Servo(picon, 0))
+            {
+                try
+                {
+                    Console.WriteLine("Enter servo value");
+                    while (running)
+                    {
+                        Console.Write("> ");
+                        var value = Console.ReadLine();
+                        if (value == "quit")
+                        {
+                            break;
+                        }
+                        if (int.TryParse(value, out var number))
+                        {
+                            servo.Angle = number;
+                        }
+                    }
+
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    return -1;
+                }
+                finally
+                {
+                    Console.WriteLine("Bye!");
+                    servo.Reset();
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/ServoTest/Servo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Devices._4tronix;
+
+namespace ServoTest
+{
+    public class Servo : IDisposable
+    {
+        private readonly int _steerPin;
+        private readonly PiconZeroBoard _picon;
+        private int _angle;
+
+        public int Angle
+        {
+            get => _angle;
+            set
+            {
+                _angle = value;
+                _picon.SetOutput(_steerPin, value);
+                Console.WriteLine($"Angle: {value}");
+            }
+        }
+
+        public Servo(PiconZeroBoard picon, int steerPin)
+        {
+            _steerPin = steerPin;
+            picon.SetOutputConfig(_steerPin, OutputType.Pwm);
+            _picon = picon;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Angle = 90;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
