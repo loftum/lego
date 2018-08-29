a8365031e1d0423ca7e8973dc55a722faa9595b8
Hans Olav <loftum@gmail.com>
2018-08-29 22:05:22 +0200

    Steering Wheel

---
+++ dotnet/LctpTestServer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LCTP;
+using LCTP.Server;
+
+namespace LctpTestServer
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("LCTP Test Server v0.0");
+            using (var source = new CancellationTokenSource())
+            {
+                Console.CancelKeyPress += (s, e) => source.Cancel();
+                try
+                {
+                    Run(source.Token).Wait(source.Token);
+                    return 0;
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Exiting ...");
+                }
+                catch (AggregateException e)
+                {
+                    Console.WriteLine(e.InnerException);
+                    return -1;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return -1;
+                }
+                finally
+                {
+                    Console.WriteLine("Bye!");
+                }
+            }
+            return 0;
+        }
+
+        private static async Task Run(CancellationToken cancellationToken)
+        {
+            using(var server = new LctpServer(LctpServer.DefaultPort, new EchoController()))
+            {
+                await server.Start(cancellationToken);
+            }
+        }
+    }
+
+    public class EchoController : IController
+    {
+        public void ConnectionClosed()
+        {
+
+        }
+
+        public void ConnectionOpened()
+        {
+
+        }
+
+        public Task<ResponseMessage> Execute(RequestMessage request)
+        {
+            return Task.FromResult(new ResponseMessage
+            {
+                Content = request.Content
+            });
+        }
+    }
+}
---
+++ dotnet/SteeringWheel/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LCTP.Client;
+using LCTP.Server;
+using UIKit;
+
+namespace SteeringWheel
+{
+    public partial class ViewController : UIViewController
+    {
+        private LctpClient _client;
+
+        public ViewController(): base("ViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void ConnectButton_TouchUpInside(UIButton sender)
+        {
+            if (string.IsNullOrWhiteSpace(HostField.Text))
+            {
+                return;
+            }
+            var parts = HostField.Text.Split(':');
+            var host = parts[0];
+            var port = parts.Length > 1 && int.TryParse(parts[1], out var v) ? v : LctpServer.DefaultPort;
+            _client = new LctpClient(host, port);
+        }
+    }
+}
+
