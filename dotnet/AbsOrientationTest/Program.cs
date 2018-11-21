594b7e1738778d5346371628d54ab9600641b1fe
Hans Olav <loftum@gmail.com>
2018-11-21 21:36:29 +0100

    AbsOrientation

---
+++ dotnet/AbsOrientationTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Devices.Adafruit.BNO055;
+using Unosquare.RaspberryIO;
+
+namespace AbsOrientationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = Pi.I2C)
+            {
+                var o = new AbsOrientation(bus);
+                
+
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
---
+++ dotnet/Devices/Adafruit/BNO055/AbsOrientation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace Devices.Adafruit.BNO055
+{
+    public enum OperationMode
+    {
+        CONFIG = 0X00,
+        ACCONLY = 0X01,
+        MAGONLY = 0X02,
+        GYRONLY = 0X03,
+        ACCMAG = 0X04,
+        ACCGYRO = 0X05,
+        MAGGYRO = 0X06,
+        AMG = 0X07,
+        IMUPLUS = 0X08,
+        COMPASS = 0X09,
+        M4G = 0X0A,
+        NDOF_FMC_OFF = 0X0B,
+        NDOF = 0X0C
+    }
+
+    public class AbsOrientation
+    {
+        public const int Id = 0xA0;
+        public const int DefaultI2CAddress = 0x29;
+        public const int AlternativeI2CAddress = 0x29;
+
+        private readonly I2CDevice _device;
+
+        public AbsOrientation(I2CBus bus)
+        {
+            _device = bus.AddDevice(DefaultI2CAddress);
+            var id = _device.ReadAddressByte((int) Registers.BNO055_CHIP_ID_ADDR);
+            if (id != Id)
+            {
+                throw new ApplicationException($"Unexpected id. Expected {Id}, but got {id}");
+            }
+
+            SetOperationMode(OperationMode.CONFIG);
+        }
+
+        private void SetOperationMode(OperationMode mode)
+        {
+            _device.WriteAddressByte((int)Registers.BNO055_OPR_MODE_ADDR, (byte)mode);
+        }
+    }
+}
