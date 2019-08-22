706e850158e28cd4a8f9e163b583313af586dea8
Hans Olav <loftum@gmail.com>
2019-08-22 21:52:04 +0200

    Graphics-schmaphics

---
+++ dotnet/Visualizer/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using AppKit;
+using Foundation;
+
+namespace Visualizer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        private readonly MainWindowController _controller = new MainWindowController();
+
+        public AppDelegate()
+        {
+            
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            _controller.Window.MakeKeyAndOrderFront(this);
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
