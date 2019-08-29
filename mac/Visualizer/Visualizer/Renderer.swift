b4446f7b2f46c7cf5339f7bdbc8a69c7c5fef82b
Hans Olav <loftum@gmail.com>
2019-08-29 21:02:24 +0200

    More metal

--- mac/Visualizer/Visualizer/Renderer.swift
+++ mac/Visualizer/Visualizer/Renderer.swift
@@ -8,6 +8,7 @@ class Renderer: NSObject, MTKViewDelegate {
     let commandQueue: MTLCommandQueue
     let pipelineState: MTLRenderPipelineState
     let vertexBuffer: MTLBuffer
+    var vertices: [Vertex]
 
     init(view: MTKView) {
         self.device = view.device!
@@ -22,7 +23,7 @@ class Renderer: NSObject, MTKViewDelegate {
             fatalError("\(error)")
         }
 
-        let vertices = [Vertex(color: [1, 0, 0, 1], pos: [-1, -1]),
+        vertices = [Vertex(color: [1, 0, 0, 1], pos: [-1, -1]),
                         Vertex(color: [0, 1, 0, 1], pos: [0, 1]),
                         Vertex(color: [0, 0, 1, 1], pos: [1, -1])]
         guard let buffer = device.makeBuffer(bytes: vertices, length: vertices.count * MemoryLayout<Vertex>.stride, options: []) else {
@@ -47,6 +48,53 @@ class Renderer: NSObject, MTKViewDelegate {
     }
 
     func draw(in view: MTKView) {
+
+        for ii in 0..<vertices.count {
+            var vertex = vertices[ii]
+            print("hello \(vertex.pos)")
+            var x = vertex.pos.x
+            var y = vertex.pos.y
+            if x <= -1 {
+                x = -1
+                if y < 1 {
+                    y += 0.01
+                }
+                else {
+                    x += 0.01
+                }
+            }
+            else if y >= 1 {
+                y = 1
+                if x < 1 {
+                    x += 0.01
+                }
+                else {
+                    y -= 0.01
+                }
+            }
+            else if x >= 1 {
+                x = 1
+                if y > -1 {
+                    y -= 0.01
+                }
+                else {
+                    x -= 0.01
+                }
+            }
+            else if y <= -1 {
+                y = -1
+                if x > -1 {
+                    x -= 0.01
+                }
+                else {
+                    y += 0.01
+                }
+            }
+
+            vertex.pos = [x, y]
+            vertices[ii] = vertex
+            print("\(vertex.pos)")
+        }
 
         guard let commandBuffer = commandQueue.makeCommandBuffer(),
               let renderPassDescriptor = view.currentRenderPassDescriptor else {
