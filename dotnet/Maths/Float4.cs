fe4328463845b9d68108d315a9dfe3c5707b1c85
loftum <hans.olav.loftum@nrk.no>
2020-01-08 08:26:31 +0100

    Float4x4

---
+++ dotnet/Maths/Float4.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Maths
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Float4
+    {
+        public static Float4 UnitX = new Float4(1f, 0f, 0f, 0f);
+        public static Float4 UnitY = new Float4(0f, 1f, 0f, 0f);
+        public static Float4 UnitZ = new Float4(0f, 0f, 1f, 0f);
+        public static Float4 UnitW = new Float4(0f, 0f, 0f, 1f);
+        public static Float4 One = new Float4(1f, 1f, 1f, 1f);
+        public static Float4 Zero = new Float4(0f, 0f, 0f, 0f);
+        public static readonly int SizeInBytes = Marshal.SizeOf<Float4>();
+        
+        public float X;
+        public float Y;
+        public float Z;
+        public float W;
+
+        public Float4(float x, float y, float z, float w)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            W = w;
+        }
+        
+        public static Float4 operator +(Float4 left, Float4 right)
+        {
+            left.X += right.X;
+            left.Y += right.Y;
+            left.Z += right.Z;
+            left.W += right.W;
+            return left;
+        }
+
+        public static Float4 operator -(Float4 left, Float4 right)
+        {
+            left.X -= right.X;
+            left.Y -= right.Y;
+            left.Z -= right.Z;
+            left.W -= right.W;
+            return left;
+        }
+
+        public static Float4 operator -(Float4 vec)
+        {
+            vec.X = -vec.X;
+            vec.Y = -vec.Y;
+            vec.Z = -vec.Z;
+            vec.W = -vec.W;
+            return vec;
+        }
+
+        public static Float4 operator *(Float4 vec, float scale)
+        {
+            vec.X *= scale;
+            vec.Y *= scale;
+            vec.Z *= scale;
+            vec.W *= scale;
+            return vec;
+        }
+
+        public static Float4 operator *(float scale, Float4 vec)
+        {
+            vec.X *= scale;
+            vec.Y *= scale;
+            vec.Z *= scale;
+            vec.W *= scale;
+            return vec;
+        }
+
+        public static Float4 operator /(Float4 vec, float scale)
+        {
+            var num = 1f / scale;
+            vec.X *= num;
+            vec.Y *= num;
+            vec.Z *= num;
+            vec.W *= num;
+            return vec;
+        }
+
+        public static bool operator ==(Float4 left, Float4 right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Float4 left, Float4 right)
+        {
+            return !left.Equals(right);
+        }
+
+        [CLSCompliant(false)]
+        public static unsafe explicit operator float*(Float4 v)
+        {
+            return &v.X;
+        }
+
+        public static unsafe explicit operator IntPtr(Float4 v)
+        {
+            return (IntPtr) (void*) &v.X;
+        }
+
+        public bool Equals(Float4 other)
+        {
+            return X.Equals(other.X) && Y.Equals(other.Y) && Z.Equals(other.Z) && W.Equals(other.W);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Float4 other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z, W);
+        }
+
+        public override string ToString()
+        {
+            return $"[{X},{Y},{Z},{W}]";
+        }
+    }
+}
---
+++ dotnet/Maths/Float4x4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Maths
+{
+    
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Float4x4
+    {
+        public static Float4x4 Identity => new Float4x4(Float4.UnitX, Float4.UnitY, Float4.UnitZ, Float4.UnitW);
+        
+        public Float4 Col0;
+        public Float4 Col1;
+        public Float4 Col2;
+        public Float4 Col3;
+        
+        public Float4 Row0 => new Float4(Col0.X, Col1.X, Col2.X, Col3.X);
+        public Float4 Row1 => new Float4(Col0.Y, Col1.Y, Col2.Y, Col3.Y);
+        public Float4 Row2 => new Float4(Col0.Z, Col1.Z, Col2.Z, Col3.Z);
+        public Float4 Row3 => new Float4(Col0.W, Col1.W, Col2.W, Col3.W);
+
+        public Float4x4(Float4 col0, Float4 col1, Float4 col2, Float4 col3)
+        {
+            Col0 = col0;
+            Col1 = col1;
+            Col2 = col2;
+            Col3 = col3;
+        }
+        
+        public Float4x4(
+            float m00,
+            float m01,
+            float m02,
+            float m03,
+            float m10,
+            float m11,
+            float m12,
+            float m13,
+            float m20,
+            float m21,
+            float m22,
+            float m23,
+            float m30,
+            float m31,
+            float m32,
+            float m33)
+        {
+            Col0 = new Float4(m00, m10, m20, m30);
+            Col1 = new Float4(m01, m11, m21, m31);
+            Col2 = new Float4(m02, m12, m22, m32);
+            Col3 = new Float4(m03, m13, m23, m33);
+        }
+
+        
+        
+        
+    }
+}
